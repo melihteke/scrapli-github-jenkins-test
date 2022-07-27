2e3232b5a18275463048ac4342e02992ee98cef1
melihteke <36086368+melihteke@users.noreply.github.com>
2022-07-27 13:50:34 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,5 +2,5 @@ from nike_devices import NikeCiscoDevice
 from pprint import pprint
 
 
-device = NikeCiscoDevice("192.168.178.1", "jenkins", "jenkins")
+device = NikeCiscoDevice("192.168.178.1", "admin", "!mlh1985")
 pprint(device.get_interface_brief())

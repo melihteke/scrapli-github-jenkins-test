1116f21078d28a55005d39b90cbc74b27205d4c8
melihteke <36086368+melihteke@users.noreply.github.com>
2022-07-27 13:38:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from scrapli.driver.core import IOSXEDriver
+
+my_device = {
+    "host": "192.168.178.1",
+    "auth_username": "jenkins",
+    "auth_password": "jenkins",
+    "auth_strict_key": False,
+    "transport": "paramiko"
+}
+
+with IOSXEDriver(**my_device) as conn:
+    print(conn.get_prompt())
+	
+	
